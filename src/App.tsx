ac9d87ce9c1849bf351c01836a2e93dadd078ed8
DaPrato4 <daprato363@gmail.com>
2025-03-04 00:29:37 +0100

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { ReactNode, useEffect, useState } from 'react';
 
 import { MyResponsiveLine } from './components/InteractiveLine';
-import { MyResponsiveBar } from './components/ResponsiveBar';
+// import { MyResponsiveBar } from './components/ResponsiveBar';
 
 import {
   Table,
@@ -20,302 +20,143 @@ import {
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
-const tempChar1Date = [
-  {
-    "country": "AD",
-    "hot dog": 53,
-    "hot dogColor": "hsl(10, 70%, 50%)",
-    "burger": 142,
-    "burgerColor": "hsl(2, 70%, 50%)",
-    "sandwich": 130,
-    "sandwichColor": "hsl(310, 70%, 50%)",
-    "kebab": 145,
-    "kebabColor": "hsl(267, 70%, 50%)",
-    "fries": 154,
-    "friesColor": "hsl(44, 70%, 50%)",
-    "donut": 179,
-    "donutColor": "hsl(351, 70%, 50%)"
-  },
-  {
-    "country": "AE",
-    "hot dog": 41,
-    "hot dogColor": "hsl(311, 70%, 50%)",
-    "burger": 166,
-    "burgerColor": "hsl(243, 70%, 50%)",
-    "sandwich": 198,
-    "sandwichColor": "hsl(215, 70%, 50%)",
-    "kebab": 74,
-    "kebabColor": "hsl(155, 70%, 50%)",
-    "fries": 51,
-    "friesColor": "hsl(123, 70%, 50%)",
-    "donut": 94,
-    "donutColor": "hsl(100, 70%, 50%)"
-  },
-  {
-    "country": "AF",
-    "hot dog": 45,
-    "hot dogColor": "hsl(226, 70%, 50%)",
-    "burger": 37,
-    "burgerColor": "hsl(329, 70%, 50%)",
-    "sandwich": 3,
-    "sandwichColor": "hsl(43, 70%, 50%)",
-    "kebab": 46,
-    "kebabColor": "hsl(166, 70%, 50%)",
-    "fries": 99,
-    "friesColor": "hsl(202, 70%, 50%)",
-    "donut": 85,
-    "donutColor": "hsl(209, 70%, 50%)"
-  },
-  {
-    "country": "AG",
-    "hot dog": 199,
-    "hot dogColor": "hsl(60, 70%, 50%)",
-    "burger": 159,
-    "burgerColor": "hsl(118, 70%, 50%)",
-    "sandwich": 74,
-    "sandwichColor": "hsl(341, 70%, 50%)",
-    "kebab": 177,
-    "kebabColor": "hsl(49, 70%, 50%)",
-    "fries": 181,
-    "friesColor": "hsl(257, 70%, 50%)",
-    "donut": 92,
-    "donutColor": "hsl(188, 70%, 50%)"
-  },
-  {
-    "country": "AI",
-    "hot dog": 15,
-    "hot dogColor": "hsl(35, 70%, 50%)",
-    "burger": 173,
-    "burgerColor": "hsl(248, 70%, 50%)",
-    "sandwich": 81,
-    "sandwichColor": "hsl(224, 70%, 50%)",
-    "kebab": 106,
-    "kebabColor": "hsl(239, 70%, 50%)",
-    "fries": 64,
-    "friesColor": "hsl(36, 70%, 50%)",
-    "donut": 199,
-    "donutColor": "hsl(175, 70%, 50%)"
-  },
-  {
-    "country": "AL",
-    "hot dog": 100,
-    "hot dogColor": "hsl(56, 70%, 50%)",
-    "burger": 152,
-    "burgerColor": "hsl(294, 70%, 50%)",
-    "sandwich": 10,
-    "sandwichColor": "hsl(72, 70%, 50%)",
-    "kebab": 198,
-    "kebabColor": "hsl(93, 70%, 50%)",
-    "fries": 18,
-    "friesColor": "hsl(246, 70%, 50%)",
-    "donut": 151,
-    "donutColor": "hsl(176, 70%, 50%)"
-  },
-  {
-    "country": "AM",
-    "hot dog": 117,
-    "hot dogColor": "hsl(298, 70%, 50%)",
-    "burger": 40,
-    "burgerColor": "hsl(91, 70%, 50%)",
-    "sandwich": 112,
-    "sandwichColor": "hsl(287, 70%, 50%)",
-    "kebab": 115,
-    "kebabColor": "hsl(286, 70%, 50%)",
-    "fries": 185,
-    "friesColor": "hsl(134, 70%, 50%)",
-    "donut": 159,
-    "donutColor": "hsl(255, 70%, 50%)"
-  }
-]
+// const tempChar1Date = [
+//   {
+//     "country": "AD",
+//     "hot dog": 53,
+//     "hot dogColor": "hsl(10, 70%, 50%)",
+//     "burger": 142,
+//     "burgerColor": "hsl(2, 70%, 50%)",
+//     "sandwich": 130,
+//     "sandwichColor": "hsl(310, 70%, 50%)",
+//     "kebab": 145,
+//     "kebabColor": "hsl(267, 70%, 50%)",
+//     "fries": 154,
+//     "friesColor": "hsl(44, 70%, 50%)",
+//     "donut": 179,
+//     "donutColor": "hsl(351, 70%, 50%)"
+//   },
+//   {
+//     "country": "AE",
+//     "hot dog": 41,
+//     "hot dogColor": "hsl(311, 70%, 50%)",
+//     "burger": 166,
+//     "burgerColor": "hsl(243, 70%, 50%)",
+//     "sandwich": 198,
+//     "sandwichColor": "hsl(215, 70%, 50%)",
+//     "kebab": 74,
+//     "kebabColor": "hsl(155, 70%, 50%)",
+//     "fries": 51,
+//     "friesColor": "hsl(123, 70%, 50%)",
+//     "donut": 94,
+//     "donutColor": "hsl(100, 70%, 50%)"
+//   },
+//   {
+//     "country": "AF",
+//     "hot dog": 45,
+//     "hot dogColor": "hsl(226, 70%, 50%)",
+//     "burger": 37,
+//     "burgerColor": "hsl(329, 70%, 50%)",
+//     "sandwich": 3,
+//     "sandwichColor": "hsl(43, 70%, 50%)",
+//     "kebab": 46,
+//     "kebabColor": "hsl(166, 70%, 50%)",
+//     "fries": 99,
+//     "friesColor": "hsl(202, 70%, 50%)",
+//     "donut": 85,
+//     "donutColor": "hsl(209, 70%, 50%)"
+//   },
+//   {
+//     "country": "AG",
+//     "hot dog": 199,
+//     "hot dogColor": "hsl(60, 70%, 50%)",
+//     "burger": 159,
+//     "burgerColor": "hsl(118, 70%, 50%)",
+//     "sandwich": 74,
+//     "sandwichColor": "hsl(341, 70%, 50%)",
+//     "kebab": 177,
+//     "kebabColor": "hsl(49, 70%, 50%)",
+//     "fries": 181,
+//     "friesColor": "hsl(257, 70%, 50%)",
+//     "donut": 92,
+//     "donutColor": "hsl(188, 70%, 50%)"
+//   },
+//   {
+//     "country": "AI",
+//     "hot dog": 15,
+//     "hot dogColor": "hsl(35, 70%, 50%)",
+//     "burger": 173,
+//     "burgerColor": "hsl(248, 70%, 50%)",
+//     "sandwich": 81,
+//     "sandwichColor": "hsl(224, 70%, 50%)",
+//     "kebab": 106,
+//     "kebabColor": "hsl(239, 70%, 50%)",
+//     "fries": 64,
+//     "friesColor": "hsl(36, 70%, 50%)",
+//     "donut": 199,
+//     "donutColor": "hsl(175, 70%, 50%)"
+//   },
+//   {
+//     "country": "AL",
+//     "hot dog": 100,
+//     "hot dogColor": "hsl(56, 70%, 50%)",
+//     "burger": 152,
+//     "burgerColor": "hsl(294, 70%, 50%)",
+//     "sandwich": 10,
+//     "sandwichColor": "hsl(72, 70%, 50%)",
+//     "kebab": 198,
+//     "kebabColor": "hsl(93, 70%, 50%)",
+//     "fries": 18,
+//     "friesColor": "hsl(246, 70%, 50%)",
+//     "donut": 151,
+//     "donutColor": "hsl(176, 70%, 50%)"
+//   },
+//   {
+//     "country": "AM",
+//     "hot dog": 117,
+//     "hot dogColor": "hsl(298, 70%, 50%)",
+//     "burger": 40,
+//     "burgerColor": "hsl(91, 70%, 50%)",
+//     "sandwich": 112,
+//     "sandwichColor": "hsl(287, 70%, 50%)",
+//     "kebab": 115,
+//     "kebabColor": "hsl(286, 70%, 50%)",
+//     "fries": 185,
+//     "friesColor": "hsl(134, 70%, 50%)",
+//     "donut": 159,
+//     "donutColor": "hsl(255, 70%, 50%)"
+//   }
+// ]
+
 const tempChartDate = [
 
   {
-    "id": "japan",
+    "id": "Media Voto",
     "color": "hsl(27, 70%, 50%)",
     "data": [
       {
-        "x": "plane",
-        "y": 253
+        "x": 2023,
+        "y": 8
       },
       {
-        "x": "helicopter",
-        "y": 239
+        "x": 2024,
+        "y": 7
       },
       {
-        "x": "boat",
-        "y": 264
-      },
-      {
-        "x": "train",
-        "y": 295
-      },
-      {
-        "x": "subway",
-        "y": 132
-      },
-      {
-        "x": "bus",
-        "y": 67
-      },
-      {
-        "x": "car",
-        "y": 64
-      },
-      {
-        "x": "moto",
-        "y": 216
-      }
-    ]
-  },
-  {
-    "id": "france",
-    "color": "hsl(283, 70%, 50%)",
-    "data": [
-      {
-        "x": "plane",
-        "y": 35
-      },
-      {
-        "x": "helicopter",
+        "x": 2025,
         "y": 5
       },
       {
-        "x": "boat",
-        "y": 96
+        "x": 2026,
+        "y": 6
       },
       {
-        "x": "train",
-        "y": 199
+        "x": 2027,
+        "y": 3
       },
       {
-        "x": "subway",
-        "y": 114
-      },
-      {
-        "x": "bus",
-        "y": 201
-      },
-      {
-        "x": "car",
-        "y": 50
-      },
-      {
-        "x": "moto",
-        "y": 156
-      }
-    ]
-  },
-  {
-    "id": "us",
-    "color": "hsl(276, 70%, 50%)",
-    "data": [
-      {
-        "x": "plane",
-        "y": 256
-      },
-      {
-        "x": "helicopter",
-        "y": 216
-      },
-      {
-        "x": "boat",
-        "y": 204
-      },
-      {
-        "x": "train",
-        "y": 234
-      },
-      {
-        "x": "subway",
-        "y": 248
-      },
-      {
-        "x": "bus",
-        "y": 182
-      },
-      {
-        "x": "car",
-        "y": 199
-      },
-      {
-        "x": "moto",
-        "y": 238
-      }
-    ]
-  },
-  {
-    "id": "germany",
-    "color": "hsl(265, 70%, 50%)",
-    "data": [
-      {
-        "x": "plane",
-        "y": 57
-      },
-      {
-        "x": "helicopter",
-        "y": 212
-      },
-      {
-        "x": "boat",
-        "y": 116
-      },
-      {
-        "x": "train",
-        "y": 119
-      },
-      {
-        "x": "subway",
-        "y": 173
-      },
-      {
-        "x": "bus",
-        "y": 184
-      },
-      {
-        "x": "car",
-        "y": 130
-      },
-      {
-        "x": "moto",
-        "y": 125
-      }
-    ]
-  },
-  {
-    "id": "norway",
-    "color": "hsl(46, 70%, 50%)",
-    "data": [
-      {
-        "x": "plane",
-        "y": 175
-      },
-      {
-        "x": "helicopter",
-        "y": 151
-      },
-      {
-        "x": "boat",
-        "y": 283
-      },
-      {
-        "x": "train",
-        "y": 90
-      },
-      {
-        "x": "subway",
-        "y": 276
-      },
-      {
-        "x": "bus",
-        "y": 118
-      },
-      {
-        "x": "car",
-        "y": 296
-      },
-      {
-        "x": "moto",
-        "y": 260
+        "x": 2028,
+        "y": 7
       }
     ]
   }
@@ -352,14 +193,14 @@ function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [years, setYears] = useState<Number[]>([]);
 
-    
-      useEffect(() => {
-        const getMovies = async () => {
-          const data = await fetchFromApi("/trending/movie/week");
-          setMovies(data);
-        };
-        getMovies();
-      }, []);
+  useEffect(() => {
+    const getMovies = async () => {
+      const data = await fetchFromApi("/trending/movie/week");
+      setMovies(data);
+    };
+    getMovies();
+  }, []);
+  console.log(movies[0]);
 
   return (
     <>
@@ -422,34 +263,34 @@ function App() {
         <h1 className="relative top-2 left-0 m-4 text-black">Grafico interattivo</h1>
         <span className="flex justify-between space-x-4">
           <div id="Selezione-Anno" className="w-1/3">
-            <label htmlFor="year" className="text-black">Seleziona Anno:</label>
+            <h2 className='text-black mb-3'>Seleziona Anno:</h2>
             <form 
-              className="flex flex-row space-y-4 text-black"
+              className="flex flex-row space-x-4 text-black justify-center mb-4"
               onSubmit={(e) => {
               e.preventDefault();
               const yearInput = (e.target as HTMLFormElement).year as HTMLInputElement;
               const year = parseInt(yearInput.value);
-              console.log(year);
-              (!years.includes(year) && (year/1000)<10)? setYears([...years, year]): alert('Anno già presente');
+              (!years.includes(year) && year < 2100 && year > 1900) ? setYears([...years, year]) : alert('Anno non valido o già presente');
               }}
             >
               <input type="number" id="year" name="year" className="border rounded px-2 py-1" placeholder="Inserisci anno" />
               <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 cursor-pointer">Invia</button>
             </form>
-            <div className="flex flex-col">
+
+            <div className="flex flex-col items-center">
               {years.map((year) => (
-              <div key={year.toString()} className="flex items-center text-black">
+                <div key={year.toString()} className="flex items-center text-white justify-center bg-blue-900 p-1.5 w-21 m-2 rounded">
                 <span>{year.toString()}</span>
                 <button 
-                className="ml-2 text-red-500"
+                className="ml-2 text-white bg-red-500 rounded w-7.5 h-7.5 text-2xl cursor-pointer leading-0 hover:bg-red-700"
                 onClick={() => {
                   setYears(years.filter(y => y !== year));
-                  console.log(years);
+                  // console.log(years);
                 }}
                 >
                 x
                 </button>
-              </div>
+                </div>
               ))}
             </div>
            
